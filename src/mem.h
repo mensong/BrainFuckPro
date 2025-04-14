b14e8795529fc7aea8199f005041edcbc62227c9
Gergul <coolcn@qq.com>
2025-04-15 01:24:57 +0800

    +file

--- src/mem.h
+++ src/mem.h
@@ -2,7 +2,7 @@
 #define BRAINFUCK_MEM_H
 
 #define calloc(n, size) (bf_safe_calloc((n), (size)))
-#define free(p) ((p != NULL) ? free(p) : NULL)
+//#define free(p) ((p != NULL) ? free(p) : NULL)
 void *bf_safe_calloc(size_t n, size_t size);
 
 #endif //BRAINFUCK_MEM_H
